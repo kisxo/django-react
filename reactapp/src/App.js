324d30bbc71f2eaba2018750c5439dd92bcd9ae7
kisxo <udaysubba2004@gmail.com>
2024-10-24 23:13:08 +0530

    restarted react app
    Reason = Rahul's chat gpt code

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,23 @@
-import "./App.css";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
-import Home from "./Components/Home";
-import Signup from "./Components/Signup";
-import Login from "./Components/Login";
-// import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Login/>
-        <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/home" element={<Home/>} />
-        </Routes>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
