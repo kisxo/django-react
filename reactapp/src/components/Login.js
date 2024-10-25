cfd7ece23ba28046bd74a8563448321069fd330f
RahulSaikia-web <rahulsaikia682@gmail.com>
2024-10-25 19:37:59 +0530

    Login & SignUp complete

--- reactapp/src/components/Login.js
+++ reactapp/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../css/Login.css'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 function Login() {
   const navigate = useNavigate();
   const handleSubmit = event =>{
@@ -9,18 +9,22 @@ function Login() {
   };
   return (
     <form onSubmit={handleSubmit}>
-    
-    <div className="container">
-      <label htmlFor="uname"><b>Username</b></label>
-      <input required type="text"  placeholder="Enter Username" />
-  
-      <label htmlFor="psw"><b>Password</b></label>
-      <input required type="password" placeholder="Enter Password" />
-  
-      <button type="submit">Login</button>
-    
-    </div>
-  
+      <div className="container">
+      <h1>Login</h1>
+    <span>Email</span>    
+
+      <div class="wrapper">
+      <input required type="email" id="" name="email" placeholder=" "/>
+</div>
+<span>Password</span>    
+
+<div class="wrapper">
+      <input required type="password" id="" name="email" placeholder=" "/>
+</div>
+      <div className="btn"><button type="submit">Login</button></div>
+      <p>New User ? <Link to="/signup"  style={{ textDecoration: 'none' }}>Signup</Link></p>
+      </div>
+
   </form>
 
   )
--- reactapp/src/components/Signup.js
+++ reactapp/src/components/Signup.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import '../css/Signup.css'
 
 function Signup() {
+  const navigate = useNavigate();
+  const handleSubmit = event =>{
+      event.preventDefault();
+      navigate('/');
+  };
   return (
-    <div><h1>Signup</h1></div>
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+      <h1>Sign Up</h1>
+    <span>Email</span>    
+
+      <div class="wrapper">
+      <input required type="email" id="" name="email" placeholder=" "/>
+</div><span>Set Username</span>    
+
+<div class="wrapper">
+<input required type="text" id="" name="username" placeholder=" "/>
+</div>
+<span>Password</span>    
+
+<div class="wrapper">
+      <input required type="password" id="" name="password" placeholder=" "/>
+</div><span>Confirm Password</span>    
+
+<div class="wrapper">
+      <input required type="password" id="" name="cPassword" placeholder=" "/>
+</div>
+     <div className="btn"> <button type="submit">Sign Up</button></div>
+      <p>Already Have an Account ? <Link to="/"  style={{ textDecoration: 'none' }}>Login</Link></p>
+      </div>
+
+  </form>
 
   )
 }
